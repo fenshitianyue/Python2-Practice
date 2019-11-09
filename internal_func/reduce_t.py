8b0834158517bb6f95a335b460b2bca07d74cbf7
fenshitianyue <abzanda@163.com>
2019-11-09 14:41:25 +0800

    add reduce function

---
+++ internal_func/reduce_t.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+nums = [1, 2, 3, 4, 5]
+
+# result = 1
+# for num in nums:
+#     result *= num
+# print result
+
+result = reduce(lambda x, y: x * y, nums)
+
+print result
+
