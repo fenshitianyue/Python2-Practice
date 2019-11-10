3b90c96653efdc99031bfe1d99244bec4dd6219b
fenshitianyue <abzanda@163.com>
2019-11-10 15:31:07 +0800

    update join method

---
+++ str/join_t.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# 用+实现字符串相加
+s1 = 'hello'
+s2 = 'world'
+
+print s1 + ' ' + s2
+
+print '--------------------'
+
+# 用join可以这么做:
+
+tmp = [s1, s2]
+print ' '.join(tmp)
