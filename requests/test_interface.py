34947fb951c3ef0114dae9a619607dd067fc8f80
fenshitianyue <abzanda@163.com>
2019-11-10 17:52:12 +0800

    update request method

---
+++ requests/test_interface.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import requests
+import json
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+interface_url = 'http://127.0.0.1:8000/web/10000/singleCase/caseRecordCounts'
+param_list = {
+    'values': json.dumps({"zone": "lag_df", "p_id": 10000}),
+    'types': 'caton'
+}
+
+def print_get_case_counts():
+    cookie = requests.cookies.RequestCookieJar()
+    cookie.set('token', 'something')
+    requests.packages.urllib3.disable_warnings()
+    result = requests.get(interface_url, params=param_list, cookies=cookie, verify=False)
+    print result.url
+    print result.content
+
+if __name__ == '__main__':
+    print_get_case_counts()
