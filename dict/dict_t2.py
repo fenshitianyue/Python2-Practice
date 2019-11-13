2a3cb87ab13f919f06b01b3bd587fe71530f3306
fenshitianyue <abzanda@163.com>
2019-11-13 20:47:50 +0800

    test dict

--- dict/dict_t2.py
+++ dict/dict_t2.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-data = {}
-
 num = 1
 
+
+data = {}
+
+# print data.get('ProductId')
 
 for i in range(10):
     if i+1 in data.keys():
@@ -13,7 +15,7 @@ for i in range(10):
         data.update({i+1: []})
         data[i+1].append(10)
 
-print data
+# print data
 
 if num in data.keys():
     data[num].append(10)
@@ -21,5 +23,8 @@ else:
     data.update({num: []})
     data[num].append(10)
 
-print data
+# print data
+
+for key in data.keys():
+    print key, ':', data[key]
 
