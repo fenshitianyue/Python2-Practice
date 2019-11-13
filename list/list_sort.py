c1dbad30085b488a640030c804997e37139aa5a5
fenshitianyue <abzanda@163.com>
2019-11-13 21:05:18 +0800

    test sort list

---
+++ list/list_sort.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+data = []
+
+for i in range(10):
+    item = dict()
+    item.update({'num': i+1})
+    item.update({'child': []})
+    data.append(item)
+
+# def takeKey(elem):
+#     return elem['num']
+
+# data.sort(key=takeKey, reverse=True)
+
+data.sort(key=lambda elem: elem['num'], reverse=True)
+
+print data
