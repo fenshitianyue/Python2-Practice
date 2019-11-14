e1fc5fe54e20597bc337d8962b38fea5dbc17a80
fenshitianyue <abzanda@163.com>
2019-11-14 20:11:27 +0800

    Find the position where the string appears

---
+++ str/find_nth.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def find_nth(source, target, num):
+    start = source.find(target)
+    while start >= 0 and num > 1:
+        start = source.find(target, start+1)
+        num -= 1
+    return start
+
+
+s = '10-11-12-'
+t = '-'
+
+print s[0:find_nth(s, t, 1)]
+print s[0:find_nth(s, t, 2)]
+print s[0:find_nth(s, t, 3)]
