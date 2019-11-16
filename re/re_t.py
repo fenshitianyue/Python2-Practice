80b5134be3b137aa351da174f3b127e061758bee
fenshitianyue <abzanda@163.com>
2019-11-16 16:45:43 +0800

    考虑到更多的异常情况，更新代码

--- re/re_t.py
+++ re/re_t.py
@@ -11,18 +11,22 @@ import re
 #     return start
 
 
-attention_name = 'ls;tombie'
+attention_name = 'nels;tombie'
 
-attention_name_2 = 'tombie;ls'
+attention_name_2 = 'tombie;nels'
 
 attention_name_3 = 'varoamfemh;nelsontang'
 
-name = 'ls'
+attention_name_4 = 'timenels;antfang'
+
+attention_name_5 = 'fnelsion;tombie'
+
+name = 'nels'
 
 if __name__ == '__main__':
-    pattern = '{0};|;{1}|;{2};'.format(name, name, name)
+    pattern = '^{0};|;{1}$|;{2};'.format(name, name, name)
     print pattern
-    result = re.search(pattern, attention_name)
+    result = re.search(pattern, attention_name_5)
     if result is not None:
         print result.span(0)
     else:
