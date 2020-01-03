265fbe89c25ce605d985056643804f7a0da5c9aa
fenshitianyue <abzanda@163.com>
2020-01-03 15:25:28 +0800

    update code

--- str/find_nth.py
+++ str/find_nth.py
@@ -13,5 +13,6 @@ s = '10-11-12-'
 t = '-'
 
 print s[0:find_nth(s, t, 2)]
+print s[0:find_nth(s, t, 1)]
 print s[find_nth(s, t, 1)+1:find_nth(s, t, 2)]
 print s[find_nth(s, t, 2)+1:find_nth(s, t, 3)]
